7159f9912321da73d4af935372bf9e17d434ea81
Felipe Clé <94229656+FelipeCle@users.noreply.github.com>
2025-02-27 14:00:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('❤ Machine Learning teste')
 
 st.write('Hello world!')
+df =pd.read_csv('https://raw.githubusercontent.com/acanellafilho/ia/refs/heads/main/Trabalho%20IA%20Parte%202/data_preProcess2.csv')
+df
