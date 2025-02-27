3c808962cd16b5b3b1e0cf023d82634dd0740bd9
Felipe Clé <94229656+FelipeCle@users.noreply.github.com>
2025-02-27 14:10:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ st.title('❤ Machine Learning teste')
 st.write('Hello world!')
 df =pd.read_csv('https://raw.githubusercontent.com/acanellafilho/ia/refs/heads/main/Trabalho%20IA%20Parte%202/data_preProcess2.csv')
 df
+
+with st.sidebar:
+  st.header('Input features')
+  # ESCOLARI,IDADE
+  escolari = set.selectbox('Escolaridade',('1','2','3','4','5'))
