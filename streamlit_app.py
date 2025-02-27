4c7e12f991a4c85fcc67b97f3a457d1d8f95e069
Felipe Clé <94229656+FelipeCle@users.noreply.github.com>
2025-02-27 15:04:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,29 @@
 import streamlit as st
 import pandas as pd
+import joblib
+
+# Carregando Modelo Teste
+model = joblib.load('random_forest_model.pkl')
 
 st.title('❤ Machine Learning teste')
 
 st.write('Hello world!')
-df =pd.read_csv('https://raw.githubusercontent.com/acanellafilho/ia/refs/heads/main/Trabalho%20IA%20Parte%202/data_preProcess2.csv')
-df
+df = pd.read_csv('https://raw.githubusercontent.com/acanellafilho/ia/refs/heads/main/Trabalho%20IA%20Parte%202/data_preProcess2.csv')
+st.write(df)
 
 with st.sidebar:
   st.header('Input features')
-  # ESCOLARI,IDADE
-  escolari = st.selectbox('Escolaridade',('1','2','3','4','5'))
+  # Input features: ESCOLARI, IDADE
+  escolari = st.selectbox('Escolaridade', ('1', '2', '3', '4', '5'))
   idade = st.slider('Idade(anos)', 0, 99, 46, step=1)
-  #Criar df com base no input
-  data = {'ESCOLARI': escolari,
-          'IDADE': idade
-          }
-  input_df = pd.DataFrame(data, index=[0])
-  input_df
+  
+  # Create a dataframe based on user input
+  data = {'ESCOLARI': [escolari],
+          'IDADE': [idade]}
+  input_df = pd.DataFrame(data)
+  st.write("Input DataFrame", input_df)
+
+  # Make prediction
+  if st.button('Predict'):
+      prediction = model.predict(input_df)
+      st.write(f"Estimated `vivo_ano5`: {prediction[0]}")
